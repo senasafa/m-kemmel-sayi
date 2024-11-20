dfaf9db6be008a3ed279f71fbe5fa7179c240dce
senasafa <senasafa8@gmail.com>
2024-11-20 23:41:50 +0300

    Add files via upload

---
+++ mükemmel sayı.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+Console.Write("bir sayı girin: ");
+int sayi =
+    Convert.ToInt32(Console.ReadLine());
+
+int toplam = 0;
+for (int i = 1; i < sayi; i++)
+{
+    if (sayi % i == 0)
+    {
+        toplam += i;
+
+
+    }
+}
+
+if (toplam == sayi)
+{
+
+    Console.WriteLine("mükemmel bir sayıdır");
+
+}
+
+else
+{
+
+    Console.WriteLine("mükemmel bir sayı değildir");
+
+
+}
